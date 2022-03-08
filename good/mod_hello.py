5be4f3874b9d0c2dc822f32f8a2c541e46b760e2
Mark Borrow <99207355+shiftleft-mark@users.noreply.github.com>
2022-03-08 11:15:54 -0500

    Update mod_hello.py

--- good/mod_hello.py
+++ good/mod_hello.py
@@ -6,20 +6,4 @@ mod_hello = Blueprint('mod_hello', __name__, template_folder='templates')
 def do_hello():
     return 'hello :)'
 
-#Mark and Mike test code
-
-import hashlib
-import os
-
-salt = os.urandom(32) # Remember this
-password = 'password123'
-
-key = hashlib.pbkdf2_hmac(
-    'sha256', # The hash digest algorithm for HMAC
-    password.encode('utf-8'), # Convert the password to bytes
-    salt, # Provide the salt
-    100000 # It is recommended to use at least 100,000 iterations of SHA-256 
-)
-
-#end code
 
